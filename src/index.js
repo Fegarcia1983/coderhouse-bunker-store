29b8c8b95fe7549b88a9a656a404d60c4626cfc3
Federico Garcia <fegarcia.1983@gmail.com>
2021-01-06 21:12:52 -0300

    App component restored

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NavBar from './components/navbar/NavBar';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
